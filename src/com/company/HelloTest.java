b45be76bd662c95ee645842bb38b68c8f6cff6ca
hupeng <hupeng@adinnet.cn>
2018-03-08 11:14:07 +0800

    test2

---
+++ src/com/company/HelloTest.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Date;
+
+/**
+ * @author Administrator
+ * @date 2018/3/8 0008
+ */
+public class HelloTest {
+    public static void main(String[] args) {
+        System.out.println("hello " + new Date());
+
+        System.out.println("test01");
+    }
+}

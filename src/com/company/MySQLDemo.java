d871a7b1c7f0c1cbc0a733d12f13cbfa5ee0827f
hupeng <hupeng@adinnet.cn>
2018-03-12 13:36:26 +0800

    sqlConnection

---
+++ src/com/company/MySQLDemo.java
@@ -0,0 +1,61 @@
+package com.company;
+
+import java.sql.*;
+
+/**
+ * @author Administrator
+ * @date 2018/3/12 0012
+ */
+public class MySQLDemo {
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/dbone";
+
+    static final String USER = "root";
+    static final String PASSWORD = "root";
+
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("连接数据库...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+
+            System.out.println("实例化Statment对象...");
+            stmt = conn.createStatement();
+            String sql;
+            sql = "select id,name,url from websites";
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String url = rs.getString("url");
+                System.out.println("id: " + id);
+                System.out.println("name: " + name);
+                System.out.println("url: " + url);
+                System.out.println("\n");
+            }
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null) stmt.close();
+            } catch (SQLException se) {
+
+            }
+            try {
+                if (conn != null) conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+        System.out.println("query over");
+    }
+}

0f4ff67cc0898432ab5a9334aab0c7a51d99152c
hupeng <hupeng@adinnet.cn>
2018-03-14 18:06:07 +0800

    update

--- src/com/company/JunitTest.java
+++ src/com/company/JunitTest.java
@@ -2,6 +2,9 @@ package com.company;
 
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Administrator
  * @date 2018/3/12 0012
@@ -17,11 +20,39 @@ public class JunitTest {
         }
     }
 
+    @Test
+    public void testTwo() {
+        int[] arr = {1, 23, 3, 45, 4};
+        for (int i :
+                arr) {
+            System.out.println("value of arr is " + i);
+        }
+    }
+
     private String CheckLogin() {
         String s1 = "www.baidu.com";
         String s2 = "www.baidu.com";
         String s3 = "www.baidu.com";
         String s4 = "www.baidu.com";
+        Map<String, String> map = new HashMap<String, String>();
+
+        for (int i = 1; i <= 5; i++) {
+            String name = "s" + i;
+            System.out.println(name);
+            map.put(name, "www.baidu.com");
+        }
+
+        System.out.println("map...");
+        for (String key :
+                map.keySet()) {
+            System.out.println("key: " + key + " value: " + map.get(key));
+        }
+
+        for (int i = 1; i <= 5; i++) {
+            String name = "s" + i;
+            System.out.println(new String("www.baidu.com"));
+        }
+
         int a = 1;
         if (a > 0) {
             System.out.println("登录成功...");
@@ -31,5 +62,4 @@ public class JunitTest {
             return "login fail...";
         }
     }
-
 }
